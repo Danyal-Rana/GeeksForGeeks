4ef013225e9d29900032cd650be44d12664693c3
Muhammad Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2023-10-19 19:52:24 +0500

    Updated Solution - GfG to GitHub

--- Medium/Next Greater Element/next-greater-element.cpp
+++ Medium/Next Greater Element/next-greater-element.cpp
@@ -4,38 +4,40 @@ using namespace std;
 
 
 // } Driver Code Ends
-// User Function Template for C++ solution
-
 class Solution
 {
-public:
-    vector<long long> nextLargerElement(vector<long long> &arr, int n)
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n)
     {
         stack<long long> myStack;
-        vector<long long> myVector(n);
-
-        for (int i = n - 1; i >= 0; i--)
+        myStack.push (-1);
+        vector<long long> myVector (n);
+        
+        for (int i=n-1; i>=0; i--)
         {
             long long currentNum = arr[i];
-
+            
             while (!myStack.empty() && myStack.top() <= currentNum)
             {
                 myStack.pop();
             }
-
-            if (!myStack.empty()) {
-                myVector[i] = myStack.top();
-            } else {
+            
+            if (myStack.empty())
+            {
                 myVector[i] = -1;
             }
-
-            myStack.push(currentNum);
+            else
+            {
+                myVector[i] = myStack.top();
+            }
+            
+            myStack.push (currentNum);
         }
-
+        
         return myVector;
     }
 };
-
 
 //{ Driver Code Starts.
 
@@ -52,13 +54,11 @@ int main()
         for(int i=0;i<n;i++)
             cin>>arr[i];
         
-        Solution ob;
-        
-        vector <long long> res = ob.nextLargerElement(arr, n);
+        Solution obj;
+        vector <long long> res = obj.nextLargerElement(arr, n);
         for (long long i : res) cout << i << " ";
         cout<<endl;
     }
 	return 0;
 }
-
 // } Driver Code Ends
